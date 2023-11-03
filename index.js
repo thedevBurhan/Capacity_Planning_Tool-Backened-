7690d33b5381bd4df48feb61135683e037ae7925
thedevBurhan <smdburhan25@gmail.com>
2023-11-03 17:46:02 +0530

    chnages in router

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use("/toDoListdata",isAuthenticated,ToDoListdataRouter)
 app.use("/timeSheet",isAuthenticated,TimeSheetdataRouter)
  
 //zoom
-app.get('/zoom', (req, res) => {
+app.get('/', (req, res) => {
 
     // Step 1: 
     // Check if the code parameter is in the url 
