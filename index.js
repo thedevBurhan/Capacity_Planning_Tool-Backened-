2fd69978c53912e509ea8b972f3b07601172bc67
thedevBurhan <smdburhan25@gmail.com>
2024-01-22 16:44:01 +0530

    video call test

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import { isAuthenticated } from './Authentication/Auth.js';
 import { usersRouter } from './Routers/Routers-User.js';
 import { ToDoListdataRouter } from './Routers/Routers-To-Do-list.js';
 import { TimeSheetdataRouter } from './Routers/Routers-Time-Sheet.js';
-import { zoomRouter } from './Routers/Routers-Zoom.js';
+
 
 dotenv.config();
 
@@ -22,77 +22,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/users', usersRouter);
 app.use('/toDoListdata', isAuthenticated, ToDoListdataRouter);
 app.use('/timeSheet', isAuthenticated, TimeSheetdataRouter);
-app.use('/', zoomRouter);
 
-// CONSTANTS
-const ZOOM_GET_AUTHCODE = 'https://zoom.us/oauth/token?grant_type=authorization_code&code=';
-const ZOOM_AUTH = 'https://zoom.us/oauth/authorize?response_type=code&client_id=';
 
-const sendErrorResponse = (res, errorMessage) => {
-  console.error(errorMessage);
-  res.status(500).json({ error: 'Internal Server Error' });
-};
 
-const sendJsonResponse = (res, data) => {
-  const JSONResponse = `<pre><code>${JSON.stringify(data, null, 2)}</code></pre>`;
-  res.send(`
-    <style>
-      /* Your CSS styles here */
-    </style>
-    <div class="container">
-      <div class="response">
-        User API Response
-        ${JSONResponse}
-      </div>
-    </div>
-  `);
-};
-
-app.get('/', async (req, res) => {
-  const authCode = req.query.code;
-
-  if (authCode) {
-    try {
-      // Request an access token using the auth code
-      const url = `${ZOOM_GET_AUTHCODE}${authCode}&redirect_uri=${process.env.redirectURL}`;
-      const { data } = await axios.post(url, {
-        client_id: process.env.clientID,
-        client_secret: process.env.clientSecret,
-      });
-
-      const { access_token, refresh_token } = data;
-
-      // Obtained access and refresh tokens
-      console.log(`Zoom OAuth Access Token: ${access_token}`);
-      console.log(`Zoom OAuth Refresh Token: ${refresh_token}`);
-
-      if (access_token) {
-        // Use the obtained access token to authenticate API calls
-        const userResponse = await axios.get('https://api.zoom.us/v2/users/me', {
-          headers: {
-            Authorization: `Bearer ${access_token}`,
-          },
-        });
-
-        const userData = userResponse.data;
-        sendJsonResponse(res, userData);
-      } else {
-        sendErrorResponse(res, 'Access token not obtained.');
-      }
-    } catch (error) {
-      sendErrorResponse(res, `Error handling Zoom callback: ${error.message}`);
-    }
-  } else {
-    // If no auth code is obtained, redirect to Zoom OAuth for authentication
-    res.redirect(`${ZOOM_AUTH}${process.env.clientID}&redirect_uri=${process.env.redirectURL}`);
-  }
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-
-// import express from "express";
 // import request from "request";
 // import bodyParser from "body-parser";
 // import cors from "cors";
@@ -159,7 +91,7 @@ app.listen(PORT, () => {
 //                         @import url('https://fonts.googleapis.com/css?family=Open+Sans:400,600&display=swap');@import url('https://necolas.github.io/normalize.css/8.0.1/normalize.css');html {color: #232333;font-family: 'Open Sans', Helvetica, Arial, sans-serif;-webkit-font-smoothing: antialiased;-moz-osx-font-smoothing: grayscale;}h2 {font-weight: 700;font-size: 24px;}h4 {font-weight: 600;font-size: 14px;}.container {margin: 24px auto;padding: 16px;max-width: 720px;}.info {display: flex;align-items: center;}.info>div>span, .info>div>p {font-weight: 400;font-size: 13px;color: #747487;line-height: 16px;}.info>div>span::before {content: "👋";}.info>div>h2 {padding: 8px 0 6px;margin: 0;}.info>div>p {padding: 0;margin: 0;}.info>img {background: #747487;height: 96px;width: 96px;border-radius: 31.68px;overflow: hidden;margin: 0 20px 0 0;}.response {margin: 32px 0;display: flex;flex-wrap: wrap;align-items: center;justify-content: space-between;}.response>a {text-decoration: none;color: #2D8CFF;font-size: 14px;}.response>pre {overflow-x: scroll;background: #f6f7f9;padding: 1.2em 1.4em;border-radius: 10.56px;width: 100%;box-sizing: border-box;}
 //                     </style>
 //                     <div class="container">
-                      
+
 //                         <div class="response">
 //                             User API Response
 //                             ${JSONResponse}
@@ -241,7 +173,7 @@ app.listen(PORT, () => {
 //   try {
 //     const { code } = req.query;
 
-   
+
 //     if (!code) {
 //       console.error("Code parameter missing");
 //       return res.status(400).send("Code parameter missing");
@@ -256,26 +188,26 @@ app.listen(PORT, () => {
 //         code: code,
 //         redirect_uri: redirectURL,
 //       };
-  
+
 //       const base64Credentials = Buffer.from(`${clientID}:${clientSecret}`).toString('base64');
 //       const headers = {
 //         Authorization: `Basic ${base64Credentials}`,
 //       };
-  
+
 //       const response = await axios.post(url, null, { params: data, headers });
 //       console.log("Token Response:", response.data);
 //       if (response.data && response.data.access_token) {
 //         const apiUrl = "https://api.zoom.us/v2/users/me";
-  
+
 //         const zoomUser = await axios.get(apiUrl, {
 //           headers: {
 //             Authorization: `Bearer ${response.data.access_token}`,
 //           },
 //         });
-  
+
 //         const zoomUserData = zoomUser.data;
 //         console.log("API call ", zoomUserData);
-  
+
 //         res.json(zoomUserData);
 //       } else {
 //         console.error("Invalid token response:", response.data);
