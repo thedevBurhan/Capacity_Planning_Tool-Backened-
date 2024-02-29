3839c40a3b3cfa9c1727205d1eb117fe2a92c33b
thedevBurhan <smdburhan25@gmail.com>
2024-02-29 21:23:45 +0530

    chat

--- Chat/Messages.js
+++ Chat/Messages.js
@@ -16,7 +16,7 @@ async function generateNewMessagesData(req, res) {
         }
 
         // Create a new conversation if conversationId is 'new' and receiverId is provided
-        if (conversationId === '' && receiverId) {
+        if (conversationId == undefined && receiverId) {
             const CreatingConversationId = new ObjectId();
             const newConversationResult = await conversation([
                 {
