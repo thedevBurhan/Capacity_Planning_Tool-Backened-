22e0d915b69ee84ed0e71606a0418700e2f49a33
thedevBurhan <smdburhan25@gmail.com>
2024-03-21 14:09:14 +0530

    socket url

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use('/chat', isAuthenticated, ConversationdataRouter);
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000", //clienturl
+        origin: "https://capacity-planningtool.netlify.app", //clienturl
         methods: ["GET", "POST", 'PUT', 'DELETE'],
     }
 })
