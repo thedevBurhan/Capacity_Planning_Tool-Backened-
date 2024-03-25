8f7311f4631030b25b8117f1c2260ddb17224358
thedevBurhan <smdburhan25@gmail.com>
2024-03-25 15:26:59 +0530

    cors

--- index.js
+++ index.js
@@ -19,25 +19,7 @@ app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-// Add headers before the routes are defined
-app.use(function (req, res, next) {
- 
-    // Website you wish to allow to connect
-    res.setHeader("Access-Control-Allow-Origin", "*")
- 
-    // Request methods you wish to allow
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE")
- 
-    // Request headers you wish to allow
-    res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type,Authorization")
- 
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader("Access-Control-Allow-Credentials", true)
- 
-    // Pass to next layer of middleware
-    next()
-})
+
 app.use('/users', usersRouter);
 app.use('/toDoListdata', isAuthenticated, ToDoListdataRouter);
 app.use('/timeSheet', isAuthenticated, TimeSheetdataRouter);
